8ab69600c76dba35fb224d9a23826249e4af6a3c
jtimog <128836706+jtimog@users.noreply.github.com>
2024-05-02 10:45:18 -0500

    Added Chapter 1 and Chapter 2

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,17 @@
+let myAge = 25;
+let year = 2019;
+
+// log things to console
+// console.log(myAge, year);
+
+age = 30;
+
+console.log(age);
+
+const points = 100;
+// points = 50;
+console.log(points);
+
+var score = 75;
+console.log(score);
+
